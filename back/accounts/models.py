f7ce385a695eb3387fe52d9dde555f2fe55db4c7
Eidard <Eidard179@gmail.com>
2021-02-04 18:38:17 +0900

    edit account model
    edit account model's field

--- back/accounts/models.py
+++ back/accounts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=32, unique=True, null=False)
+    password = models.CharField(max_length=128, null=False)
+    name = models.CharField(max_length=18, null=False)
+    email = models.EmailField(max_length=128, unique=True, null=False)
+    generation = models.IntegerField(null=False)
+    student_id = models.CharField(max_length=12, unique=True, null=False)
+    gender = models.CharField(max_length=6, null=False)
+    birth = models.DateTimeField()
+    phone = models.CharField(max_length=13)
+    joined_at = models.DateTimeField(auto_now_add=True)
+    is_approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['name', 'generation', 'student_id']
