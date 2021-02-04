97eddef1ac1f7b79585a90028e74e2d3ec5c1d47
yjlee0235 <yjlee0235@naver.com>
2021-02-04 19:39:40 +0900

    connect to AWS RDS mysql server

--- back/config/settings.py
+++ back/config/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pocs',
+        'USER': 'admin',
+        'PASSWORD': 'pocs1234',
+        'HOST': 'pocs-dev-mysql.cavcoi84nbul.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '3306',
     }
 }
 
