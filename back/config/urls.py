e21243f348ebfb9b442f65a01dee56d8d0b564ba
yjlee0235 <yjlee0235@naver.com>
2021-03-09 12:58:54 +0900

    make index view

--- back/config/urls.py
+++ back/config/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    # path('', include('accounts.urls')),
+    path('', include('index.urls')),
     path('admin/', admin.site.urls),
     path('users/', include('accounts.urls')),
     path('board/', include('board.urls')),
--- back/index/views.py
+++ back/index/views.py
@@ -1,11 +1,40 @@
 import json
 
-from django.shortcuts import render
 from django.views import View
+from django.http import JsonResponse
 
+from board.models import Board
+from board.serializers import BoardSerializer, BoardCategorySerializer
 
-# Create your views here.
+from posts.models import Post
+from posts.serializers import PostSerializerInIndex
+
+from accounts.serializers import AccountSerializerInPost
+
 
 class IndexView(View):
     def get(self, request):
-        pass
+        boards = Board.objects.all()
+        response_data = []
+        for board in boards:
+            board_data = BoardSerializer(board).data
+            board_data['category'] = BoardCategorySerializer(board.board_category).data
+            posts_data = []
+            posts = Post.objects.filter(board_id=board.id)[:5]
+            for post in posts:
+                post_data = PostSerializerInIndex(post).data
+                author_data = AccountSerializerInPost(post.author_id).data
+                post_data['author'] = author_data
+                posts_data.append(post_data)
+            board_data['posts'] = posts_data
+            temp = board_data['category']
+            parent_category_obj = board.board_category.parent
+            while parent_category_obj is not None:
+                parent_category = BoardCategorySerializer(parent_category_obj).data
+                temp['parent_category'] = parent_category
+                temp = temp['parent_category']
+                parent_category_obj = parent_category_obj.parent
+            response_data.append(board_data)
+
+        response_data = {"boards":response_data}
+        return JsonResponse(response_data, status=200)
